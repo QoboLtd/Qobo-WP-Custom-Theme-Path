478f273dd4299a1ac21af4aac543ac38f9395baf
Andreas Demetriou <a.demetriou@qobo.biz>
2014-11-25 08:13:52 +0000

    plugin creation

---
+++ qobo-custom-theme-path.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Qobo Custom Themes Path
+ * Plugin URI: 
+ * Description: Registers the a custom themes path for better theme organization.
+ * Version: 1.0.0
+ * Author: Andreas Demetriou
+ * License: GPL2
+ */
+
+register_theme_directory( ABSPATH  . '../custom-themes');
