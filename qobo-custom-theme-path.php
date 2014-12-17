0ad163fbdac41c9652176dd3c6b2c105d2300892
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-12-17 18:37:59 +0200

    Make sure stylesheet_root option has the correct value

--- qobo-custom-theme-path.php
+++ qobo-custom-theme-path.php
@@ -3,9 +3,20 @@
  * Plugin Name: Qobo Custom Theme Path
  * Plugin URI:  https://github.com/QoboLtd/Qobo-WP-Custom-Theme-Path 
  * Description: Registers the a custom theme path for better theme organization.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Andreas Demetriou
  * License: GPL2
  */
 
-register_theme_directory( ABSPATH  . '../custom-themes');
+$qoboCustomThemeDirectory = ABSPATH . '../custom-themes';
+
+// Register custom-theme directory for "on the fly"
+register_theme_directory($qoboCustomThemeDirectory);
+
+// Check if the option is preserved for later too
+if (get_option('stylesheet_root') <> $qoboCustomThemeDirectory) {
+	update_option('stylesheet_root', $qoboCustomThemeDirectory);
+}
+
+// No need for variable anymore
+unset($qoboCustomThemeDirectory);
