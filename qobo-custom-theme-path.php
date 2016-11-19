657da9d6b9cb4092a6dac690c7f50bcba606d98d
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-11-19 18:17:42 +0200

    Automatic CS fixes with phpcbf

--- qobo-custom-theme-path.php
+++ qobo-custom-theme-path.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Qobo Custom Theme Path
- * Plugin URI:  https://github.com/QoboLtd/Qobo-WP-Custom-Theme-Path 
+ * Plugin URI:  https://github.com/QoboLtd/Qobo-WP-Custom-Theme-Path
  * Description: Registers the a custom theme path for better theme organization.
  * Version: 1.0.1
  * Author: Andreas Demetriou
@@ -9,19 +9,19 @@
  */
 
 // Allow to change this in wp-config.php or elsewhere
-if (!defined('QB_CUSTOM_THEME_PATH')) {
-	define('QB_CUSTOM_THEME_PATH', '../custom-themes');
+if ( ! defined( 'QB_CUSTOM_THEME_PATH' ) ) {
+	define( 'QB_CUSTOM_THEME_PATH', '../custom-themes' );
 }
 
 $qoboCustomThemePath = ABSPATH . QB_CUSTOM_THEME_PATH;
 
 // Register custom-theme path for "on the fly"
-register_theme_directory($qoboCustomThemePath);
+register_theme_directory( $qoboCustomThemePath );
 
 // Check if the option is preserved for later too
-if (get_option('stylesheet_root') <> $qoboCustomThemePath) {
-	update_option('stylesheet_root', $qoboCustomThemePath);
+if ( get_option( 'stylesheet_root' ) <> $qoboCustomThemePath ) {
+	update_option( 'stylesheet_root', $qoboCustomThemePath );
 }
 
 // No need for variable anymore
-unset($qoboCustomThemePath);
+unset( $qoboCustomThemePath );
