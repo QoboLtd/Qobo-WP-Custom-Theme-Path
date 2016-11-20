d148d0184a9a4157ade67e48a1d8235862488bf8
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-11-20 19:30:32 +0200

    Version bump and author updates

--- qobo-custom-theme-path.php
+++ qobo-custom-theme-path.php
@@ -3,8 +3,9 @@
  * Plugin Name: Qobo Custom Theme Path
  * Plugin URI:  https://github.com/QoboLtd/Qobo-WP-Custom-Theme-Path
  * Description: Registers the a custom theme path for better theme organization.
- * Version: 1.0.1
- * Author: Andreas Demetriou
+ * Version: 2.0.0
+ * Author: Qobo Ltd
+ * Author URI: https://www.qobo.biz
  * License: GPL2
  *
  * @package Qobo_Custom_Theme_Path
